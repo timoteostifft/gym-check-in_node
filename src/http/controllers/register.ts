9a5b941b3c276c0cf94359527356a5b73c256ab4
timoteostifft <timoteostifft@gmail.com>
2023-08-23 16:46:38 -0300

    feat: add password hash and user already exists verification

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
+import { hash } from 'bcryptjs';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body);
 
+    const userAlreadyExists = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (userAlreadyExists) {
+        return reply.status(409).send();
+    }
+
+    const password_hash = await hash(password, 6);
+
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password
+            password_hash
         }
     });
 
