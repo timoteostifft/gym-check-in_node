3230019c3f698f2e18f0773158e9fe69383ebba6
timoteostifft <timoteostifft@gmail.com>
2023-08-23 20:37:08 -0300

    feat: add global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify';
 import { routes } from './http/routes';
+import { env } from './env';
+import { ZodError } from 'zod';
 
 export const app = fastify();
 
 app.register(routes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' });
+});
