3e29e6464cc166e59ae37703dc55dd7a2571fd4b
timoteostifft <timoteostifft@gmail.com>
2023-09-13 17:07:37 -0300

    feat: add end-to-end test environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vitest/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsConfigPaths()]
+  plugins: [tsConfigPaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    dir: 'src', // Essa linha
+  },
 });
