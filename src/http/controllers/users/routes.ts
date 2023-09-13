6b8b07f102e0bd4494c6defd3e01e3ef0d315f55
timoteostifft <timoteostifft@gmail.com>
2023-09-13 19:10:10 -0300

    feat: add create gym controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from 'fastify';
-import { authenticate } from '@/http/controllers/authenticate';
-import { register } from './controllers/register';
-import { profile } from '@/http/controllers/profile';
+import { authenticate } from './authenticate';
+import { register } from './register';
+import { profile } from './profile';
 import { verifyJwt } from '@/http/middlewares/verify-jwt';
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
 
   app.post('/sessions', authenticate);
