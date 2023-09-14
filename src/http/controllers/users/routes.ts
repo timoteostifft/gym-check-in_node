494e5ce2de1c8c73d2f90289fa503d3f27b479e1
timoteostifft <timoteostifft@gmail.com>
2023-09-14 16:38:31 -0300

    feat: add refresh token route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { authenticate } from './authenticate';
 import { register } from './register';
 import { profile } from './profile';
 import { verifyJwt } from '@/http/middlewares/verify-jwt';
+import { refresh } from './refresh';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
-
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   app.get('/me', { onRequest: [verifyJwt] }, profile);
 }
