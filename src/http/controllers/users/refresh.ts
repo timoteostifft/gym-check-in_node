924aeab1fc7b40a78f51a9fdf5c52e158f53d00e
timoteostifft <timoteostifft@gmail.com>
2023-09-14 22:46:35 -0300

    feat: add role-based access control

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -3,8 +3,10 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify({ onlyCookie: true });
 
+  const { role } = request.user;
+
   const token = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
@@ -13,11 +15,12 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   );
 
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
         expiresIn: '7d',
+        
       },
     },
   );
